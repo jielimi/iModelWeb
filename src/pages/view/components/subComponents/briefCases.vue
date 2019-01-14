238a2706fc762859dd732f37c54737229515038c
menghua.ye <menghua.ye@bentley.com>
2019-01-14 09:36:36 +0800

    ifont

---
+++ src/pages/view/components/subComponents/briefCases.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>briefCases
+        <i class="iconfont icon-briefcase-1"></i>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'briefCases',
+    data () {
+        return {
+        };
+    },
+    components: {
+        
+    },
+    created () {},
+    methods: {
+    }
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+
+</style>
--- src/pages/view/components/toolBar.vue
+++ src/pages/view/components/toolBar.vue
@@ -1,21 +1,36 @@
 <template>
-    <div>toolbar!</div>
+    <div class="tool-bar">
+        <brief-cases-component></brief-cases-component>
+    </div>
 </template>
+<script>
+
+ import briefCasesComponent from './subComponents/briefCases'
 
 export default {
-  name: 'toolBar',
-  data () {
-    return {
-    };
-  },
-  components: {},
-  created () {},
-  methods: {
-  }
-};
+    name: 'toolBar',
+    data () {
+        return {
+        };
+    },
+    components: {
+        briefCasesComponent
+    },
+    created () {},
+    methods: {
+    }
+    
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
-
+.tool-bar{
+    padding: 5px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-items: center;
+}
 </style>
