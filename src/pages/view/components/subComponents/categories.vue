3b60b4f8c4411feb34c100f334a4dc553922338f
Jeremy <Jeremy.Wang@bentley.com>
2019-01-24 11:01:10 +0800

    category component

--- src/pages/view/components/subComponents/categories.vue
+++ src/pages/view/components/subComponents/categories.vue
@@ -3,7 +3,7 @@
         <i class="iconfont icon-layers-1 category" @click.self="detail">
             <div v-show="isShowDetail" class="detail">
                 <el-checkbox-group v-model="checkCodeList">
-                    <el-checkbox id="cbxCatToggleAll" label="Toggle All"></el-checkbox>
+                    <el-checkbox id="cbxCatToggleAll" label="Toggle All" @change="handleCheckAllChange"></el-checkbox>
                     <el-checkbox v-for="category in categoryList" :label="category.code" :key="category.id" @change="applyCategoryToggleChange(category.id,category.code)"></el-checkbox>
                 </el-checkbox-group>
             </div>
@@ -16,7 +16,7 @@ export default {
     name: 'briefCases',
     data () {
         return {
-            isShowDetail: true,
+            isShowDetail: false,
             categoryList: [],
             checkList: [],
             checkCodeList: []
@@ -71,7 +71,32 @@ export default {
             if (alreadyInvis !== invis){
                 view.changeCategoryDisplay(id, invis);
             }
-            view.changeCategoryDisplay(id, invis);
+            //view.changeCategoryDisplay(id, invis);
+        },
+        handleCheckAllChange(val){
+            let that = this;
+            let view = this.GLOBAL_DATA.theViewPort.view;
+            if(val){
+                this.categoryList.forEach(function(val,index){
+                    if(that.checkList.indexOf(val.id) < 0){
+                        that.checkCodeList.push(val.code);
+                        that.checkList.push(val.id);
+                        const alreadyInvis = view.viewsCategory(val.id);
+                        if (alreadyInvis !== true){
+                            view.changeCategoryDisplay(val.id, true);
+                        }
+                    }
+                });
+            }else{
+                this.checkCodeList = [];
+                this.checkList = [];
+                this.categoryList.forEach(function(val,index){
+                    const alreadyInvis = view.viewsCategory(val.id);
+                    if (alreadyInvis !== false){
+                        view.changeCategoryDisplay(val.id, false);
+                    }
+                });
+            }
         }
 
     }
