78b9adf78bf436c0b4db957f5c29c68ea3a82b2e
menghua.ye <menghua.ye@bentley.com>
2019-02-19 15:08:45 +0800

    openfile

--- src/pages/view/components/subComponents/openFile.vue
+++ src/pages/view/components/subComponents/openFile.vue
@@ -26,7 +26,8 @@ export default {
     data () {
         return {
             dialogVisible:false,
-            inputFileUrl:''
+            inputFileUrl:'',
+            theViewPort:undefined,
         };
     },
     components: {
