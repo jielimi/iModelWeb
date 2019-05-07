79f6d39e6ca8563d4547761977d36ab7cc82c285
menghua.ye <menghua.ye@bentley.com>
2019-05-07 15:12:54 +0800

    graffiti

--- src/pages/view/components/subComponents/graffiti.vue
+++ src/pages/view/components/subComponents/graffiti.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        <i class="iconfont icon-qianbi" @click="draw">
+    <div >
+        <i v-if="drawGraffiti" class="iconfont icon-qianbi" @click="draw">
+        </i>
+         <i v-else class="iconfont icon-qianbi" @click="stop">
         </i>
     </div>
 </template>
@@ -14,7 +16,7 @@ export default {
     name: 'graffiti',
     data () {
         return {
-            
+            drawGraffiti:true
         };
     },
     components: {
@@ -23,11 +25,30 @@ export default {
     created () {},
     methods: {
         async draw(){
+            this.drawGraffiti = !this.drawGraffiti;
             IModelApp.tools.run("iModelWeb.graffiti")
 
             MarkupApp.props.active.element.stroke = "white"; // as an example, set default color for elements
             MarkupApp.markupSelectToolId = "iModelWeb.graffiti"; // as an example override the default markup select tool to launch redline tools using key events
-            await MarkupApp.start(this.GLOBAL_DATA.theViewPort);
+            
+            await MarkupApp.start(IModelApp.viewManager.selectedView);
+        },
+        async stop() {
+            this.drawGraffiti = !this.drawGraffiti;
+            const startMarkupSelect = IModelApp.toolAdmin.defaultToolId === MarkupApp.markupSelectToolId && (undefined === IModelApp.toolAdmin.activeTool || MarkupApp.markupSelectToolId !== IModelApp.toolAdmin.activeTool.toolId);
+            if (startMarkupSelect) {
+                IModelApp.toolAdmin.startDefaultTool();
+                return;
+            }
+            MarkupApp.props.result.maxWidth = 1500;
+            const markupData = await MarkupApp.stop();
+
+            let parent = document.getElementById("imodelview");
+            let htmlCanvas = parent.children[0].children[0];
+            let dataURL = htmlCanvas.toDataURL(); 
+            // console.log(dataURL);
+            window.open(dataURL)   
+            
         }
     }
     
