040bade59f9de624c39e02244caa0794eee6d965
menghua.ye <menghua.ye@bentley.com>
2019-05-10 14:04:18 +0800

    skill

--- src/pages/view/components/subComponents/tip.vue
+++ src/pages/view/components/subComponents/tip.vue
@@ -1,11 +1,33 @@
 <template>
     <div v-show="showToolTip" class="tip">
-    <div v-html='message'>
-        {{message}}
-    </div>
-    <div class="externalMsg">
-        Extented:{{extraMsg}}
-    </div>
+        <div class="mainMsg">
+            <div><span>Id</span>0x20000000026</div>
+            <div><span>Type</span>PhysicalObject</div>
+            <div><span>Category</span>Plan Border</div>
+            <div><span>Model</span>mydgn</div>
+        </div>
+        <el-collapse v-model="activeName" accordion>
+            <el-collapse-item title="General" name="1">
+                <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+            </el-collapse-item>
+            <el-collapse-item title="Geometry" name="2">
+                <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+            </el-collapse-item>
+            <el-collapse-item title="RawData" name="3">
+                <div>简化流程：设计简洁直观的操作流程；</div>
+                <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+            </el-collapse-item>
+            <el-collapse-item title="Material" name="4">
+                <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+            </el-collapse-item>
+            <el-collapse-item title="Extend" name="5">
+                Extented:{{extraMsg}}
+            </el-collapse-item>
+        </el-collapse>
     </div>
 </template>
 
@@ -22,7 +44,8 @@ export default {
         return {
             showToolTip:false,
             message:'',
-            extraMsg:[]
+            extraMsg:[],
+            activeName: '1'
         };
     },
     components: {
@@ -34,14 +57,14 @@ export default {
     },
     methods: {
        getExtraParam(param) {
-            this.$get('api/extraMsg',{},param).then(res=>{
+            this.$get('api/detailMsg',{},param).then(res=>{
                 if(res.state === 0){
-                    for(var i = 0; i< res.data.extraMsg.length;i++){
-                        delete res.data.extraMsg[i].className
-                        delete res.data.extraMsg[i].element
-                        delete res.data.extraMsg[i].id
-                    }
-                    this.extraMsg = res.data.extraMsg
+                    // for(var i = 0; i< res.data.extraMsg.length;i++){
+                    //     delete res.data.extraMsg[i].className
+                    //     delete res.data.extraMsg[i].element
+                    //     delete res.data.extraMsg[i].id
+                    // }
+                    // this.extraMsg = res.data.extraMsg
                 }
             })
        },
@@ -112,18 +135,51 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
+.el-collapse-item__header {
+    background-color: transparent;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    height: 40px;
+    line-height: 48px;
+    color: #fff;
+    cursor: pointer;
+    border-bottom: none;
+    font-size: 13px;
+    font-weight: 500;
+    -webkit-transition: border-bottom-color .3s;
+    transition: border-bottom-color .3s;
+    outline: 0;
+    padding-left: 5px;
+}
 .tip{
+    background-color: rgba(69,77,102,0.8);
     padding: 10px;
     text-align: left;
     position: absolute;
     top: 65px;
     left: 15px;
-    width: 150px;
+    width: 15%;
     color: white;
     opacity: 0.6;
     z-index: 1000;
     border: 2px solid;
     border-radius: 5px;
+}
+.mainMsg{
+    margin-bottom: 5px;
+    span{
+            padding-bottom: 5px;
+            width: 100px;
+            display: inline-block;
+    }
+}
+
+.subtitle{
+
 }
 
 </style>
