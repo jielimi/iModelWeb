b59f2362998a6f1385e5f4e3528270914c9a0d6b
menghua.ye <menghua.ye@bentley.com>
2019-05-30 13:45:51 +0800

    mark

--- src/pages/view/components/subComponents/graffiti.vue
+++ src/pages/view/components/subComponents/graffiti.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
-        <i v-if="drawGraffiti" class="iconfont icon-qianbi" @click="draw">
+        <i  class="iconfont icon-qianbi" @click="draw">
         </i>
-         <i v-else class="iconfont icon-qianbi" @click="stop">
-        </i>
+        
     </div>
 </template>
 
@@ -84,31 +83,25 @@ export default {
             GraffitiTool.register(toolNamespace);
         },
         async draw(){
-            this.drawGraffiti = !this.drawGraffiti;
-            IModelApp.tools.run("iModelWeb.graffiti")
-
-            MarkupApp.props.active.element.stroke = "white"; // as an example, set default color for elements
-            MarkupApp.markupSelectToolId = "iModelWeb.graffiti"; // as an example override the default markup select tool to launch redline tools using key events
-            
-            await MarkupApp.start(IModelApp.viewManager.selectedView);
-        },
-        async stop() {
-            this.drawGraffiti = !this.drawGraffiti;
-            const startMarkupSelect = IModelApp.toolAdmin.defaultToolId === MarkupApp.markupSelectToolId && (undefined === IModelApp.toolAdmin.activeTool || MarkupApp.markupSelectToolId !== IModelApp.toolAdmin.activeTool.toolId);
-            if (startMarkupSelect) {
-                IModelApp.toolAdmin.startDefaultTool();
-                return;
+           
+            if(this.drawGraffiti){
+                IModelApp.tools.run("iModelWeb.graffiti")
+                MarkupApp.props.active.element.stroke = "white"; // as an example, set default color for elements
+                MarkupApp.markupSelectToolId = "iModelWeb.graffiti"; // as an example override the default markup select tool to launch redline tools using key events
+                await MarkupApp.start(IModelApp.viewManager.selectedView);
+            }else{
+                const startMarkupSelect = IModelApp.toolAdmin.defaultToolId === MarkupApp.markupSelectToolId && (undefined === IModelApp.toolAdmin.activeTool || MarkupApp.markupSelectToolId !== IModelApp.toolAdmin.activeTool.toolId);
+                if (startMarkupSelect) {
+                    IModelApp.toolAdmin.startDefaultTool();
+                    return;
+                }
+                MarkupApp.props.result.maxWidth = 1500;
+                
+                const markupData = await MarkupApp.stop();
             }
-            MarkupApp.props.result.maxWidth = 1500;
-            
-            const markupData = await MarkupApp.stop();
-
-            // let parent = document.getElementById("imodelview");
-            // let htmlCanvas = parent.children[0].children[0];
-            // let dataURL = htmlCanvas.toDataURL(); 
-            // you can save the picture here 
-            
-        }
+            this.drawGraffiti = !this.drawGraffiti;
+        },
+        
     }
     
 }
