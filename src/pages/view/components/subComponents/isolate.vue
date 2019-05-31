f24ae927909b73f5c7151e6e5295178c211ab544
Jeremy <Jeremy.Wang@bentley.com>
2019-05-31 12:09:50 +0800

    color

---
+++ src/pages/view/components/subComponents/isolate.vue
@@ -0,0 +1,147 @@
+<template>
+    <div>
+        <i class="iconfont icon-color isolote" @click.self="pop">
+        	<div v-show="isolating" class="detail">
+        		<el-checkbox v-model="colorActive">
+        			<span>Color</span>
+        			<input type="color" :disabled="!colorActive" value="#ffffff" @change="handleColorChange">
+        		</el-checkbox>
+        		<el-checkbox v-model="transparencyActive">
+        			<span>Transparency</span>
+        			<input type="range" :disabled="!transparencyActive" class="range" min="0.0" max="1.0" step="0.05" value="0.0" @change="handleTransparencyChange">
+        		</el-checkbox>
+                <hr />
+                <el-button-group>
+					<el-button size="mini" @click="apply">Apply</el-button>
+					<el-button size="mini" @click="setDefault">Default</el-button>
+					<el-button size="mini" @click="clear">Clear</el-button>
+				</el-button-group>
+        	</div>
+        </i>
+    </div>
+</template>
+
+<script>
+import { dispose, Id64String, IDisposable } from "@bentley/bentleyjs-core";
+import { ColorByName, ColorDef, RgbColor } from "@bentley/imodeljs-common";
+import { IModelApp, EmphasizeElements,FeatureOverrideProvider, FeatureSymbology  } from "@bentley/imodeljs-frontend";
+const provider = {
+	elementOvrs: '',
+	appearance: FeatureSymbology.Appearance.defaults,
+	defaultOvrs: FeatureSymbology.Appearance | undefined,
+    addFeatureOverrides(ovrs, _vp) {
+    	this.elementOvrs.forEach((value, key) => ovrs.overrideElement(key, value));
+        // const appearance = FeatureSymbology.Appearance.fromRgba(new ColorDef(this.targetColor));
+        // this.elemSet.forEach(v => {
+        //     ovrs.overrideElement(v, appearance);
+        // });
+    }
+};
+export default {
+    name: 'isolate',
+    data () {
+        return {
+        	colorActive: false,
+        	transparencyActive: false,
+        	isolating: false
+        };
+    },
+    components: {
+        
+    },
+    created () {},
+    methods: {
+    	pop(){
+    		this.isolating = !this.isolating;
+    	},
+        handleColorChange(e){
+        	let props = provider.appearance.toJSON();
+      		props['rgb'] = this.convertHexToRgb(e.target.value);
+      		provider.appearance = FeatureSymbology.Appearance.fromJSON(props);
+        },
+        handleTransparencyChange(e){
+        	// let props = provider.appearance.toJSON();
+      		// props['transparency'] = parseFloat(e.target.value);
+      		// provider.appearance = FeatureSymbology.Appearance.fromJSON(props);
+        },
+        apply(){
+      		let elementOvrs = new Map();
+			for(const id of this.GLOBAL_DATA.theViewPort.iModel.selectionSet.elements){
+				elementOvrs.set(id, provider.appearance);
+			}
+			provider.elementOvrs = elementOvrs;
+			this.GLOBAL_DATA.theViewPort.featureOverrideProvider = provider;
+			this.sync();
+        },
+        setDefault(){
+        	let pro = this.GLOBAL_DATA.theViewPort.featureOverrideProvider;
+        	pro.defaults = provider.appearance;
+        	this.sync();
+        },
+        clear(){
+			provider.elementOvrs.clear();
+			provider.defaultOvrs = undefined;
+			this.sync();
+        },
+        sync(){
+        	this.GLOBAL_DATA.theViewPort.setFeatureOverrideProviderChanged();
+        },
+        convertHexToRgb(hex) {
+		  // Parse a hex color string formatted as "#FFFFFF"
+		  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+		  return result ? new RgbColor(
+		    parseInt(result[1], 16),
+		    parseInt(result[2], 16),
+		    parseInt(result[3], 16),
+		  ) : undefined;
+		}
+
+    }
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+	.isolote {
+        position: relative;
+        .detail {
+            position: absolute;
+            right: 0;
+            top: 45px;
+            z-index: 999;
+            width: 300px;
+            max-height: 350px;
+            overflow-y: auto;
+            padding-bottom: 10px;
+            text-align: left;
+            border: 1px solid #666;
+            background-color: #E9F2F9;
+            .el-checkbox {
+                display: block;
+                margin-left: 15px;
+                margin-top: 5px;
+            }
+        }
+        .el-checkbox {
+        	display: inline-block;
+        }
+        .range {
+        	position: relative;
+    		top: 7px;
+        }
+        hr {
+        	width: 95%;
+        	margin: 15px auto 0;;
+        	color: #ccc;
+        }
+        .el-button-group {
+        	margin-left: 20px;
+        }
+        button:focus {
+            background-color: #fff;
+            color: #606266;
+            border: 1px solid #dcdfe6;
+        }
+    }
+</style>
