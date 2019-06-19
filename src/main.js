2c3805b50ee84e3872ac7beb024aee9c5e1c3e6c
tang415a <tang415a@163.com>
2019-06-19 17:48:15 +0800

    customized HitDetail.getToolTip

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/common.less';
 import locale from 'element-ui/lib/locale/lang/en';
-
+import './polyfills';
 
 Vue.config.productionTip = false
 
