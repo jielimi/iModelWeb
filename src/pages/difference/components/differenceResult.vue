740e229b20c3cc67e67b0def3b53d532c77a8ba1
menghua.ye <menghua.ye@bentley.com>
2019-06-25 16:20:51 +0800

    yezi

--- src/pages/difference/components/differenceResult.vue
+++ src/pages/difference/components/differenceResult.vue
@@ -1,9 +1,52 @@
 <template>
     <div>
-        <el-input
+      <el-tabs type="border-card">
+        <el-tab-pane label="Add">
+            <el-table
+              :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+              height="200"
+              border
+              style="width: 100%">
+              <el-table-column
+                prop="date"
+                label="日期"
+                width="180">
+              </el-table-column>
+              <el-table-column
+                prop="name"
+                label="姓名"
+                width="180">
+              </el-table-column>
+              <el-table-column
+                prop="address"
+                label="地址">
+              </el-table-column>
+              
+              <el-table-column label="操作">
+                <template slot="header" slot-scope="scope">
+                  <el-input
+                    v-model="search"
+                    size="mini"
+                    placeholder="输入关键字搜索"/>
+                </template>
+                
+                <template slot-scope="scope">
+                  <el-button
+                    size="mini"
+                    @click="AddFocusElement(scope.row)">focus
+                  </el-button>
+                </template>
+            </el-table-column>
+            </el-table>
+         
+        </el-tab-pane>
+        <el-tab-pane label="Modify">2</el-tab-pane>
+        <el-tab-pane label="Delete">3</el-tab-pane>
+      </el-tabs>
+        <!-- <el-input
         placeholder="input keyword to filter"
         v-model="filterText">
-        </el-input>
+        </el-input> -->
         
     </div>
 </template>
@@ -14,7 +57,36 @@ export default {
     name: 'differenceResult',
      data() {
       return {
-        filterText: ''
+         search: '',
+         tableData: [{
+          date: '2016-05-03',
+          name: 'yezi1',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: 'yezi122',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '444',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '555',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-08',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-06',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-07',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }]
       }
     },
     props:['projectId','startVersionName','endVersionName'],
@@ -24,7 +96,7 @@ export default {
     created () {},
     mounted(){
         this.getResult();
-        // window.eventHub.$on('view_start_init',this.send)
+        
     },
     watch: {
       
@@ -33,16 +105,17 @@ export default {
         getResult(){
              let param = {
               projectId:this.projectId,
-              startversion:this.startversion,
-              endversion: this.endversion
+              startversion:'3',
+              endversion: '4'
             }
+           
 
-            // this.$get('api/version/difference',{}, param).then(res => {
-            //   this.isLoading = false;
-            //     if (res.state === 0) {
+            this.$get('api/version/differences',{}, param).then(res => {
+              this.isLoading = false;
+                if (res.state === 0) {
                 
-            //     }
-            // });
+                }
+            });
             
         }
     }
