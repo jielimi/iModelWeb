ab2bd7812d79539145f3661823f21bb192213cc9
menghua.ye <menghua.ye@bentley.com>
2019-09-24 14:09:08 +0800

    'yezi'

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,8 +1,11 @@
 <template>
     <div style="height: 100%;overflow: hidden;">
         <!-- <img class="img" src="../../assets/images/2.jpg"/> -->
-        <!-- <img class="img" id="img" src="../../assets/images/fa.png"/> -->
-        <canvas></canvas>
+        <img class="img" id="img" src="../../assets/images/fa.png"/>
+        <ul class="bg-bubbles">
+            <li v-for="(item, index) in bubbles" :key="index"></li>
+        </ul>
+        
         <div class="login">
             <el-form :model="loginForm" :rules="loginRules">         
             <h2>iModel Web</h2>
@@ -26,11 +29,12 @@
     </div>
 </template>
 <script>
-import animate from './3D.js'
+
 export default {
   name: "login",
   data() {
     return {
+        bubbles:[1,2,3,4,5,6,7,7,7,9,10,9],
         loginForm: {
             username: '',
             password: '',
@@ -39,9 +43,7 @@ export default {
         username: [
             {
                 required: true,
-
                 message: "please input user name",
-
                 trigger: "blur"
             }
         ],
@@ -51,15 +53,17 @@ export default {
                message: 'please input password name',
                trigger: 'blur'
            }
-        ]
+        ],
+        
       }
     }
   },
   components: {
-  
   },
   mounted () {
-      animate();
+  },
+  created() {
+    
   },
   methods: {
       start(){
@@ -73,8 +77,8 @@ export default {
         width:300px;
 }    
 .img{
-    width: 100%;
     height: auto;
+    width: 50%;
     opacity: 0.6;
 }
 .login{
@@ -90,7 +94,102 @@ export default {
     a{
         color: white;
     }
-
 }
+
+.bg-bubbles {
+    position: absolute;
+    margin: 0;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    li {
+      position: absolute;
+      bottom: -160px;
+      width: 40px;
+      height: 40px;
+      background-color: rgba(255, 255, 255, 0.5);
+      list-style: none;
+      animation: square 15s infinite;
+      transition-timing-function: linear;
+      &:nth-child(1) {
+        left: 10%;
+      }
+      &:nth-child(2) {
+        left: 20%;
+        width: 90px;
+        height: 90px;
+        animation-delay: 2s;
+        animation-duration: 7s;
+      }
+      &:nth-child(3) {
+        left: 25%;
+        animation-delay: 4s;
+      }
+      &:nth-child(4) {
+        left: 40%;
+        width: 60px;
+        height: 60px;
+        animation-duration: 8s;
+        background-color: rgba(255, 255, 255, 0.3);
+      }
+      &:nth-child(5) {
+        left: 70%;
+      }
+      &:nth-child(6) {
+        left: 80%;
+        width: 120px;
+        height: 120px;
+        animation-delay: 3s;
+        background-color: rgba(255, 255, 255, 0.2);
+      }
+      &:nth-child(7) {
+        left: 32%;
+        width: 160px;
+        height: 160px;
+        animation-delay: 2s;
+      }
+      &:nth-child(8) {
+        left: 55%;
+        width: 20px;
+        height: 20px;
+        animation-delay: 4s;
+        animation-duration: 15s;
+      }
+      &:nth-child(9) {
+        left: 25%;
+        width: 10px;
+        height: 10px;
+        animation-delay: 2s;
+        animation-duration: 12s;
+        background-color: rgba(255, 255, 255, 0.3);
+      }
+      &:nth-child(10) {
+        left: 85%;
+        width: 160px;
+        height: 160px;
+        animation-delay: 5s;
+      }
+    }
+    // 自定义 square 动画；
+    @keyframes square {
+      0% {
+        opacity: 0.5;
+        transform: translateY(0px) rotate(45deg);
+      }
+      25% {
+        opacity: 0.75;
+        transform: translateY(-400px) rotate(90deg)
+      }
+      50% {
+        opacity: 1;
+        transform: translateY(-600px) rotate(135deg);
+      }
+      100% {
+        opacity: 0;
+        transform: translateY(-1000px) rotate(180deg);
+      }
+    }
+  }
 
 </style>
