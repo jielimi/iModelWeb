b1cf64bb4b66a1827a7c9b326853abd8c385f00f
menghua.ye <menghua.ye@bentley.com>
2019-09-30 11:18:29 +0800

    'yezi'

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -6,7 +6,7 @@
             <li v-for="(item, index) in bubbles" :key="index"></li>
         </ul>
   </div>
-  <div class="login">
+  <div class="login" id="login">
             <el-form :model="loginForm" :rules="loginRules">         
             <h2>iModel Web</h2>
             <el-form-item prop="username">
@@ -14,7 +14,17 @@
             </el-form-item>
 
             <el-form-item prop="password">
-                <el-input  v-model="loginForm.password" name="password" placeholder="Password" type="password" auto-complete="on" maxlength=15></el-input>
+                <el-input  v-model="loginForm.password" name="password"  v-if="pwdType == 'password'" placeholder="Password" type="password" auto-complete="on" maxlength=15></el-input>
+                <el-input  v-model="loginForm.password" name="password"  v-else placeholder="Password" type="text" auto-complete="on" maxlength=15></el-input>
+
+                <img :src="src" @click="changeType()" class="eyes"/>
+            </el-form-item>
+
+            <el-form-item label="" prop="identityCode">
+              <div class="vertifyCode">
+                <el-input  v-model.number="loginForm.identityCode" autocomplete="off"></el-input>
+                <img @load="verifyLoadState=true"  @click="toggleVerify()" :src="img_src" alt="">
+              </div>
             </el-form-item>
             
             <el-form-item>
@@ -34,10 +44,16 @@ export default {
   name: "login",
   data() {
     return {
+        pwdType: 'password',
+        src:
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAUCAMAAABPqWaPAAAAwFBMVEUSi/cAAAA4n/kSjfcakPcolvfn8/7R6f7u9/7i8P4fkfg2n/gZkPcTjPkVjfgim/wTi/cSifYSjPcTjfgTjfj///8zjdk6ofgSivcbkvcSi/gSjPgUjPgTjfcUjfgTjPUdkPv0+v/8/v/w+f8Sivf///8Si/cRi/cSjPgTjPgTjfcTjPj///8YkPoWivgajfYgkPD///8bkfc8ofj5/f8Sivcakvj///8Si/grmPsUjPYbkvj4//8Tjvb///8bkfb/mfgJAAAAPnRSTlOAAPKG44b69fHi2vr2eTkN2tmLbGkNBeTYxKF+WFAzKBHr49PIxpSEb2VfSTsxIx0I+PLc19LQuKx8c0tIGw3kU54AAADeSURBVCjPZdHnrsIwDAVg+3LZpEA3dLD33rt9/7ciqR2pEudPIn2SlRMDyqxvDv5EiXFOJo67ieZC2NE2J5ujN0xGD+DY0liandQbJNUi6KxcEknX9K+Sp6XLYhT+icxCjkACAFG51h5PiUIlMQBRSZHPZCDsAmCqZzQmCXZgA6XbIPJn/HiItbSYWAzAkG6mz0SPXyDg3iK6tzOiXqKf/cGc510kUeWgR037T+CJJleOUYkiHqgrjxwtuA/zNJBLUULpWZpqqTc8rEnYIkuIxeojl3J6o5KfNDsvdXwBlrAYHNt6Kh8AAAAASUVORK5CYII=',
+        img_src:'',
+        verifyLoadState:true,
         bubbles:[1,2,3,4,5,6,7,7,7,9,10,9],
         loginForm: {
             username: '',
             password: '',
+            identityCode:''
         },
         loginRules: {
         username: [
@@ -50,11 +66,10 @@ export default {
         password: [
            {
                required: true,
-               message: 'please input password name',
+               message: 'please input password',
                trigger: 'blur'
            }
-        ],
-        
+        ]
       }
     }
   },
@@ -63,17 +78,42 @@ export default {
   mounted () {
   },
   created() {
-    
+    this.toggleVerify();
   },
   methods: {
+      toggleVerify(){
+        if(!this.verifyLoadState) return;
+        this.verifyLoadState =  false;
+        const base = '/api/user/identifyCode'
+        this.img_src = base + '?' + (new Date()).getTime();
+      },
       login(){
           let param = {
               username:this.loginForm.username,
-              password:this.loginForm.password
+              password:this.loginForm.password,
+              vertifyCode:this.loginForm.identityCode
           }
+
+          if(!param.username || !param.password){
+            this.$message({
+                    message:"please input your name or your password",
+                    type:'warning'
+                })
+            return;
+          }
+
+          if(!param.vertifyCode){
+            this.$message({
+                    message:"please input vertify code",
+                    type:'warning'
+                })
+            return;
+          }
+
           let that = this;
 
           this.$post('api/user/login',param).then(res=>{
+             this.toggleVerify();
              if(res.state != 0) {
                 this.$message({
                     message:res.message,
@@ -83,14 +123,21 @@ export default {
              else{
                  setCookie('token',res.data.token,3600);
                  setCookie('username',that.loginForm.username);
-                  if(that.$route.query){
+                  if(that.$route.query.redirect){
                       that.$router.push({'path':that.$route.query.redirect});
                   }else{
                       that.$router.push({'path':'/'});
                   }
              }
           })
-      }
+      },
+      changeType () {
+        this.pwdType = this.pwdType === 'password' ? 'type' : 'password';
+        this.src =
+          this.pwdType === 'password'
+            ? 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAUCAMAAABPqWaPAAAAwFBMVEUSi/cAAAA4n/kSjfcakPcolvfn8/7R6f7u9/7i8P4fkfg2n/gZkPcTjPkVjfgim/wTi/cSifYSjPcTjfgTjfj///8zjdk6ofgSivcbkvcSi/gSjPgUjPgTjfcUjfgTjPUdkPv0+v/8/v/w+f8Sivf///8Si/cRi/cSjPgTjPgTjfcTjPj///8YkPoWivgajfYgkPD///8bkfc8ofj5/f8Sivcakvj///8Si/grmPsUjPYbkvj4//8Tjvb///8bkfb/mfgJAAAAPnRSTlOAAPKG44b69fHi2vr2eTkN2tmLbGkNBeTYxKF+WFAzKBHr49PIxpSEb2VfSTsxIx0I+PLc19LQuKx8c0tIGw3kU54AAADeSURBVCjPZdHnrsIwDAVg+3LZpEA3dLD33rt9/7ciqR2pEudPIn2SlRMDyqxvDv5EiXFOJo67ieZC2NE2J5ujN0xGD+DY0liandQbJNUi6KxcEknX9K+Sp6XLYhT+icxCjkACAFG51h5PiUIlMQBRSZHPZCDsAmCqZzQmCXZgA6XbIPJn/HiItbSYWAzAkG6mz0SPXyDg3iK6tzOiXqKf/cGc510kUeWgR037T+CJJleOUYkiHqgrjxwtuA/zNJBLUULpWZpqqTc8rEnYIkuIxeojl3J6o5KfNDsvdXwBlrAYHNt6Kh8AAAAASUVORK5CYII='
+            : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAASCAMAAACZ8IWSAAAAY1BMVEUSi/cAAAASifcTjPkTjfgajvYUjfgSi/cSjPcRjPcSjPgVi/czjdkTjPgSjfcTjfgVi/cSjfgSivcSivcSivcSi/gTi/YSivcTjPgTjPgTjfcTjPgWkPYgkPASjPkSjPMUifV0GXiLAAAAIXRSTlOAANl6ahExlIqFbiUFdWBaPDjSycespZ1lVE9JHAgqKhobG1qjAAAAm0lEQVQY03WRWRbCIAwAUwioLAVcu6r3P6WtJEq1nS/CPEIWqAinWyMldpYv2CQPBNrSqBoKevcxWsKC2pFJQOwPhZpMpPB6FOK0y+cwG03iIt6QihUMnElkzhQPgHS6CYL+QtAbJkEVVrM1UwUjrlQg1Vy1Mv9V29yp8r+dap4Ov2Lid6JjW9zL+2ILD2QRFG+BsZ3xsumfHL8A2V8Fj3TEBx8AAAAASUVORK5CYII='; // 用64位编码
+    },
   }
 };
 </script>
@@ -117,12 +164,19 @@ export default {
     position: absolute;
     top: 22%;
     right: 10%;
-    width: 500px;
+    width: 550px;
     h2 {
         color:white
     }
     a{
         color: white;
+    }
+    .eyes{
+          top: 17px;
+          position: absolute;
+          height: 18px;
+          width: 25px;
+          right: 130px;
     }
 }
 
@@ -220,6 +274,14 @@ export default {
         transform: translateY(-1000px) rotate(180deg);
       }
     }
+  }
+  
+  .vertifyCode img{
+    height: 40px;
+    cursor: pointer;
+    position: absolute;
+    background-color: white;
+    width: 100px;
   }
 
 </style>
