24e06a1f3c2cf532ba6bdd5356fe4d4e14eb9b54
menghua.ye <menghua.ye@bentley.com>
2019-12-09 09:21:18 +0800

    'fps'

--- src/pages/view/components/subComponents/debugInfo.vue
+++ src/pages/view/components/subComponents/debugInfo.vue
@@ -5,13 +5,13 @@
                 class="detail"
                 title="Debug Info"
                 v-show="dialogVisible">
-                <div class="item">
+                <!-- <div class="item">
                     <el-checkbox v-model="showFPS"  @change="handleFPSCheckChange">
                         <span v-show="fpsStatus == 0">Track FPS</span>
                         <span v-show="fpsStatus == 1">Tracking FPS...</span>
                         <span v-show="fpsStatus == 2">FPS: <span>{{ fps }}</span></span>
                     </el-checkbox>
-                </div>
+                </div> -->
                 <div class="item request">
                     <span>Max Active Requests:</span>
                     <el-input-number v-model="maxActiveRequests" @change="handleRequestsChange" :min="0" :step="1" size="mini"></el-input-number>
@@ -303,29 +303,30 @@ export default {
     methods: {
         init(){
             this.maxActiveRequests = IModelApp.tileAdmin.maxActiveRequests;
-            this.debugControl = IModelApp.renderSystem.debugControl;
+            this.debugControl = JSON.parse(JSON.stringify(IModelApp.renderSystem.debugControl));
+          
         },
         detail() {
             this.dialogVisible = !this.dialogVisible;
         },
-        handleFPSCheckChange($event){
-            if($event){
-                this.fpsStatus = 1;
-                GLOBAL_DATA.theViewPort.continuousRendering = $event;
-                this._metrics = new PerformanceMetrics(false, true);
-                this._curIntervalId = setInterval(() => this.updateFPS(), 500);
-            }else{
-                this.fpsStatus = 0;
-                this._metrics = undefined;
-                this.clearInterval();
-            }
-            GLOBAL_DATA.theViewPort.target.performanceMetrics = this._metrics;
-        },
-        updateFPS(){
-            const metrics = this._metrics;
-            this.fps = (metrics.spfTimes.length / metrics.spfSum).toFixed(2);
-            this.fpsStatus = 2;
-        },
+        // handleFPSCheckChange($event){
+        //     if($event){
+        //         this.fpsStatus = 1;
+        //         GLOBAL_DATA.theViewPort.continuousRendering = $event;
+        //         this._metrics = new PerformanceMetrics(false, true);
+        //         this._curIntervalId = setInterval(() => this.updateFPS(), 500);
+        //     }else{
+        //         this.fpsStatus = 0;
+        //         this._metrics = undefined;
+        //         this.clearInterval();
+        //     }
+        //     GLOBAL_DATA.theViewPort.target.performanceMetrics = this._metrics;
+        // },
+        // updateFPS(){
+        //     const metrics = this._metrics;
+        //     this.fps = (metrics.spfTimes.length / metrics.spfSum).toFixed(2);
+        //     this.fpsStatus = 2;
+        // },
         clearInterval(){
             if (undefined !== this._curIntervalId) {
                 clearInterval(this._curIntervalId);
